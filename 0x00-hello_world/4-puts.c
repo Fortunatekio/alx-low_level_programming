c80ffbf354ab7c4c59585f11186cecb8706a6723
FortunateKio <fortunatemugendi8@gmail.com>
2023-03-16 11:37:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
 /**
  * main - prints "Programming like building a multilingual 
  * puzzlee, followed by a new line."
- * return: 0(Success)
+ * return: 0 (Success)
 */
 
-int main(void)
+int main (void)
 {
-   	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+   	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
