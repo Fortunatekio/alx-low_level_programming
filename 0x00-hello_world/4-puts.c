65fe468015f68f0090c19e2525998325761f07f4
FortunateKio <fortunatemugendi8@gmail.com>
2023-03-16 13:20:43 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  * puzzlee, followed by a new line."
  * return: 0 (Success)
 */
-int main(void)
+void main(int argc, char *argv[])
 
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
