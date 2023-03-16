b992b5afdbcb4965be5111890e3f1f519ea6ad2b
FortunateKio <fortunatemugendi8@gmail.com>
2023-03-16 15:07:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints "Programming like building a multilingual
- * puzzlee, followed by a new line."
- * return: Always 0 (success)
+ * main - Entry point
+ * Return: Always 0 (success)
 */
 
 int main(void)
