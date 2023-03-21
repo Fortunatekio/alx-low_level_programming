19d68dbd1f7ee8b42c2248ebea2f024b20a6dd5b
FortunateKio <fortunatemugendi8@gmail.com>
2023-03-21 13:02:36 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,17 @@
 void print_alphabet_x10(void)
 {
 	char letter;
+	int i = 0;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	while (i <= 0)
+	{
+
+	for (i = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
+	}
 
 	_putchar('\n');
+	i++;
+	}
 }
