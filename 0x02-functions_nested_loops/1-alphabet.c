52332b957043f5432642e37576d6f946d4ce5c8b
FortunateKio <fortunatemugendi8@gmail.com>
2023-03-21 10:25:57 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    _putchar('abcdefghijklmnopqrstuvwxyz');
+    _putchar(',');
+    return (0);
+}
