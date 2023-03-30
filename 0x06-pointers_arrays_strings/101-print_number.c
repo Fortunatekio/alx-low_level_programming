611ed999f711bd0e318f33b143fbfb78c6bee78f
FortunateKio <fortunatemugendi8@gmail.com>
2023-03-29 23:56:22 -0700

    on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,12 +13,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('_');
+		_putchar('_');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
+	return (0);
 }
