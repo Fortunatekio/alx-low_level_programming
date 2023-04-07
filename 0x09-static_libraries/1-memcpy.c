271855530e08bac36f9769b059a054158d5d204c
FortunateKio <fortunatemugendi8@gmail.com>
2023-04-06 17:43:53 -0700

    static library 0

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ * @dest: pointer to the destination array
+ * @src: pointer to the source of data to be copied
+ * @n: the number of bytes
+ * Return: returns pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int p;
+int q = n;
+
+for (p = 0; p < q; p++)
+{
+        dest[p] = src[p];
+        n--;
+}
+return (dest);
+}
