de5aad6f11ed37fccac6b04011132616b57fba32
FortunateKio <fortunatemugendi8@gmail.com>
2023-04-17 18:40:56 -0700

    main 3 dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,14 +14,14 @@ struct dog
 	char *owner;
 };
 /**
- * my_dog - the typedef
+ * dog_t - the typedef
  */
-typedef struct dog my_dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-my_dog *white_dog(char *name, float age, char *owner);
-void blue_dog(my_dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *a);
 
