4f58c01ae5bbeb81b358d7cbeed17b8f108913dc
FortunateKio <fortunatemugendi8@gmail.com>
2023-04-27 14:21:17 -0700

    add node 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,7 +17,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	while (str[len])
 		len++;
-	new = malloc(sizeof(list_t));
+	new = malloc(sizeof(*new));
 	if (!new)
 		return (NULL);
 	new->str = strdup(str);
