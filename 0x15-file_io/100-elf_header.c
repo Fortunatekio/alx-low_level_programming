2d6c3c1a0350c32c4d619a3709baa7c2667a9a8b
Fortunatekio <fortunatemugendi8@gmail.com>
2023-08-07 14:20:20 +0300

    changes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -16,12 +16,10 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-int main(int __attribute__((__unused__)) argc, char *argv[]);
 
 /**
 * check_elf - Checks if a file is an ELF file.
 * @e_ident: A pointer to an array containing the ELF magic numbers.
-*
 * Description: If the file is not an ELF file - exit code 98.
 */
 void check_elf(unsigned char *e_ident)
@@ -44,7 +42,6 @@ exit(98);
 /**
 * print_magic - Prints the magic numbers of an ELF header.
 * @e_ident: A pointer to an array containing the ELF magic numbers.
-*
 * Description: Magic numbers are separated by spaces.
 */
 void print_magic(unsigned char *e_ident)
@@ -116,7 +113,6 @@ printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 * print_version - Prints the version of an ELF header.
 * @e_ident: A pointer to an array containing the ELF version.
 */
-
 void print_version(unsigned char *e_ident)
 {
 printf(" Version: %d",
@@ -248,7 +244,6 @@ printf("%#lx\n", e_entry);
 /**
 * close_elf - Closes an ELF file.
 * @elf: The file descriptor of the ELF file.
-*
 * Description: If the file cannot be closed - exit code 98.
 */
 void close_elf(int elf)
@@ -266,19 +261,17 @@ exit(98);
 * ELF header at the start of an ELF file.
 * @argc: The number of arguments supplied to the program.
 * @argv: An array of pointers to the arguments.
-*
 * Return: 0 on success.
-*
 * Description: If the file is not an ELF File or
 * the function fails - exit code 98.
 */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 Elf64_Ehdr *header;
-int part, g;
+int o, r;
 
-part = open(argv[1], O_RDONLY);
-if (part == -1)
+o = open(argv[1], O_RDONLY);
+if (o == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
@@ -286,18 +279,19 @@ exit(98);
 header = malloc(sizeof(Elf64_Ehdr));
 if (header == NULL)
 {
-close_elf(part);
+close_elf(o);
 dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 exit(98);
 }
-g = read(part, header, sizeof(Elf64_Ehdr));
-if (g == -1)
+r = read(o, header, sizeof(Elf64_Ehdr));
+if (r == -1)
 {
 free(header);
-close_elf(part);
+close_elf(o);
 dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 exit(98);
 }
+
 check_elf(header->e_ident);
 printf("ELF Header:\n");
 print_magic(header->e_ident);
@@ -308,8 +302,9 @@ print_osabi(header->e_ident);
 print_abi(header->e_ident);
 print_type(header->e_type, header->e_ident);
 print_entry(header->e_entry, header->e_ident);
+
 free(header);
-close_elf(part);
+close_elf(o);
 return (0);
 }
 
