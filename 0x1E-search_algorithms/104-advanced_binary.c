bb9e7162b2ff397e24cd5e8b308907dbc5f63d52
Fortunatekio <fortunatemugendi8@gmail.com>
2023-12-13 19:02:35 +0300

    searches for a value

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -14,15 +14,15 @@ int rec_search(int *array, size_t size, int value)
 {
 	size_t half = size / 2;
 	size_t i;
-	
+
 	if (array == NULL || size == 0)
 		return (-1);
-	
+
 	printf("Searching in array");
-	
+
 	for (i = 0; i < size; i++)
 		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-	
+
 	printf("\n");
 
 	if (half && size % 2 == 0)
@@ -34,10 +34,10 @@ int rec_search(int *array, size_t size, int value)
 			return (rec_search(array, half + 1, value));
 		return ((int)half);
 	}
-	
+
 	if (value < array[half])
 		return (rec_search(array, half + 1, value));
-	
+
 	half++;
 	return (rec_search(array + half, size - half, value) + half);
 }
@@ -54,11 +54,11 @@ int rec_search(int *array, size_t size, int value)
 int advanced_binary(int *array, size_t size, int value)
 {
 	int index;
-	
+
 	index = rec_search(array, size, value);
-	
+
 	if (index >= 0 && array[index] != value)
 		return (-1);
-	
+
 	return (index);
 }
